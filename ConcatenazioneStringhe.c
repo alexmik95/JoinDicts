c13868276ece484ad3c6a8155b78598ee826a71b
alexmik95 <33226430+alexmik95@users.noreply.github.com>
2018-05-15 18:45:06 +0200

    Update ConcatenazioneStringhe.c

--- ConcatenazioneStringhe.c
+++ ConcatenazioneStringhe.c
@@ -6,29 +6,36 @@
  */
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define MAX_STR_LENGHT 20 
+#define MAX_STR_LENGTH 20 
 
-char* concatena(char *nomeFIleA, char *nomeFileB) {
-	FILE *listaA = fopen(“listaA.txt”, “r”);
-	FILE *listaB = fopen(“listaB.txt”, “r”);
-	FILE *listaOUT = fopen(“listaOUT.txt”, “a+”);
+char* concatena(char *nomeFileA, char *nomeFileB, char *nomeFileOut) {
+	FILE *listaA;
+	listaA = fopen(nomeFileA, "r");
+	FILE *listaB;
+	listaB = fopen(nomeFileB, "r");
+	FILE *listaOut;
+	listaOut = fopen(nomeFileOut, "a+");
 	
-	char *stringaA = malloc(sizeof(char) MAX_STR_LENGHT);
-	fscanf(listaA, "%s", &stringaA);
+	//char *stringaA = malloc(sizeof(char) MAX_STR_LENGTH);
+	char stringaA[MAX_STR_LENGTH];
+	fscanf(listaA, "%s", stringaA);
 	printf("letta stringa dal file A: %s", stringaA);
 
-	char *stringaB = malloc(sizeof(char) MAX_STR_LENGHT);
-	fscanf(listaB, "%s", &stringaB);
+	//char *stringaB = malloc(sizeof(char) MAX_STR_LENGTH);
+	char stringaB[MAX_STR_LENGTH];
+	fscanf(listaB, "%s", stringaB);
 	printf("letta stringa dal file B: %s", stringaB);
-	
-	char *strg = malloc(sizeof(char) * ((strlen(stringaA))+(strlen(stringaB) )) );		//alloca porzione di memoria per contenere la stringa di massima lunghezza la somma delle due in input
-	strg = strcat(stringaA, stringaB);
-	fprintf(listaOUT, "%s", strg);
+	int strgLength = ( (strlen(stringaA))+(strlen(stringaB)) + 1);
+	char *strg = malloc(sizeof(char) * strgLength );		//alloca porzione di memoria per contenere la stringa di massima lunghezza la somma delle due in input
+	//char strg[MAX_STR_LENGTH];
+	strcpy ( strcat(stringaA, stringaB), strg );
+	fprintf(listaOut, "%s", strg);
 
 	fclose(listaA);
 	fclose(listaB);
-	fclose(listaOUT);
+	fclose(listaOut);
 
 	return strg;
 }
@@ -37,7 +44,10 @@ char* concatena(char *nomeFIleA, char *nomeFileB) {
 
 int main() {
 	char *stringaFinale;
-	strcpy( stringaFinale, concatena("listaA", "listaB") );
+	char lA[]="listaA.txt";
+	char lB[]="listaB.txt";
+	char lO[]="listaOut.txt";
+	strcpy( stringaFinale, concatena(lA, lB, lO) );
 	printf("%s", stringaFinale);
 	return 0;
 }
